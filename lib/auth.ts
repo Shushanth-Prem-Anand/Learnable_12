e9cfa3ab9a2bea507cbb6e047f8039e71d2e0c79
Shushanth <shushanth03@gmail.com>
2023-11-21 12:45:57 +0530

    Add upload course functionality

---
+++ lib/auth.ts
@@ -0,0 +1,21 @@
+import { getServerSession } from "next-auth"
+
+export type AuthUser = {
+    name:string,
+    email:string,
+    image:string
+}
+
+export const getUserSession = async ():Promise<AuthUser> => {
+    const session = await getServerSession();
+    if(!session) throw new Error('unauthorized')
+
+    return session.user as AuthUser
+}
+
+
+export const User = async () => {
+    const session = await getServerSession();
+    return session?.user as AuthUser
+
+}
